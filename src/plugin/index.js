98e1d9b6a9be067f218b38603dae94d1d8b8f27d
JerryYu2014 <648542313@qq.com>
2020-07-28 01:05:14 +0800

    ...

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -1,8 +1,11 @@
 import * as components from './components'
-var VuePlugin = {}
+
+const VuePlugin = {}
+
 VuePlugin.install = function (Vue, options) {
   for (let component in components) {
     Vue.component('ml' + component, components[component])
   }
 }
+
 export default VuePlugin
