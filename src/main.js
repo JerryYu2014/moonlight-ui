ff600c25103233ead79cac44498c9ea17c10ff60
JerryYu2014 <648542313@qq.com>
2020-07-27 14:55:29 +0800

    ...

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-// import '@/plugin/assets/scss/index.scss'
+import '@/plugin/assets/scss/index.scss'
 import mlui from '@/plugin'
 Vue.use(mlui)
 
